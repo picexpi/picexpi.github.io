479215b45e6eff6d24ecfa56437c6454fcbaa815
M n <36516242+tiraxturumuz1@users.noreply.github.com>
2026-08-14 19:13:54 +0330

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 // frontend/src/pages/Home.tsx
 import React from 'react';
 import Navbar from '../components/Navbar';
+import LanguageSwitcher from '../components/LanguageSwitcher';
+import PiTestnetPayment from '../components/PiTestnetPayment';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
-import About from '../components/About';
 import Poll from '../components/Poll';
+import About from '../components/About';
 import Footer from '../components/Footer';
 
 const Home: React.FC = () => {
@@ -13,7 +15,17 @@ const Home: React.FC = () => {
       <Navbar />
 
       <main>
+        {/* انتخاب زبان */}
+        <LanguageSwitcher />
+
+        {/* پنل اصلی و سالم Login/Payment پای - منتقل شده به اول صفحه */}
+        <div id="pi-payment-panel">
+          <PiTestnetPayment />
+        </div>
+
+        {/* Hero بدون Login */}
         <Hero />
+
         <Features />
         <Poll />
         <About />
